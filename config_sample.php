a41a5a569ef1bff78456ea4364b7dd8e1caa16fa
Juan Lotito <jlotito@opensistemas.com>
2017-01-28 19:14:57 +0100

    creando el config_sample

---
+++ config_sample.php
@@ -0,0 +1,13 @@
+<?php
+
+define("PROJECT_PATH", "path/al/proyecto");
+define("AUDIO_PATH", PROJECT_PATH."/tmp_audios");
+define("AUDIO_FILE", AUDIO_PATH."/temp.flac");
+
+define("DB_HOSTNAME", "host");
+define("DB_USER", "username");
+define("DB_PASSWORD", "password");
+define("DB_NAME", "databasename");
+
+?>
+
